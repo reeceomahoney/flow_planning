612863be3a8be0503dd3c9221edc43693f432e3f
Reece O'Mahoney <reece.omahoney3@gmail.com>
2025-03-25 17:07:56 +0000

    change flow planning env cfg

--- flow_planning/envs/flow_planning.py
+++ flow_planning/envs/flow_planning.py
@@ -1,8 +1,10 @@
 import isaaclab.sim as sim_utils
 from isaaclab.assets.rigid_object import RigidObjectCfg
+from isaaclab.managers.manager_term_cfg import EventTermCfg
 from isaaclab.utils import configclass
 
-from .rsl_rl import FrankaRLEnvCfg
+from . import mdp
+from .rsl_rl import T_MAX, FrankaRLEnvCfg
 
 
 @configclass
@@ -11,7 +13,16 @@ class FrankaFlowPlanningEnvCfg(FrankaRLEnvCfg):
         super().__post_init__()
         self.observations.policy.pose_command = None  # type: ignore
         self.curriculum = None  # type: ignore
-        self.commands.ee_pose.fixed_commands = [(0.8, 0, 0.2), (0.8, 0, 0.6)]  # type: ignore
+        self.events.reset_robot_joints = EventTermCfg(
+            func=mdp.reset_joints_fixed, mode="reset"
+        )
+        self.commands.ee_pose = mdp.ScheduledPoseCommandCfg(  # type: ignore
+            asset_name="robot",
+            body_name="panda_hand",
+            resampling_time_range=(T_MAX, T_MAX),
+            debug_vis=True,
+            fixed_commands=[(0.8, 0, 0.2), (0.8, 0, 0.6)],
+        )
 
 
 @configclass
