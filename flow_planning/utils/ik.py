4cfbe1bf301752ec196eb653e7ea02e52594384d
Reece O'Mahoney <reece.omahoney3@gmail.com>
2025-04-11 17:11:20 +0100

    add ik tests

--- flow_planning/utils/ik.py
+++ flow_planning/utils/ik.py
@@ -52,11 +52,21 @@ class IKSolver:
 
 
 if __name__ == "__main__":
+    # fmt: off
+    test_data ={
+        (0.4, 0, 0.2): Tensor([-2.2671e-02, 7.6667e-02, 2.1580e-02, -2.7259e00, -4.9747e-03, 2.8028e00, 7.8894e-01, 9.8029e-05, 1.0173e-04]),
+        (0.6, 0, 0.6): Tensor([-1.6977, -2.267e-02, 1.3992e-01, 1.8799e-02, -1.4575e00, -2.6461e-03, 1.5982e00, 7.8706e-01, 9.3350e-05, 1.0834e-04]),
+        (0.4, 0, 0.6): Tensor([-1.7655, -2.267e-02, -4.9210e-01, 1.3407e-02, -2.1302e00, 6.3447e-03, 1.6397e00, 7.7912e-01, 9.3380e-05, 1.0829e-04]),
+        (0.6, 0, 0.2): Tensor([-2.9749, -2.267e-02, 4.9310e-01, 2.9753e-02, -2.0311e00, -2.4329e-02, 2.5243e00, 8.0171e-01, 9.6156e-05, 1.0438e-04]),
+    }
+    # fmt: on
+
     ik_solver = IKSolver()
-    start_time = time.time()
-    posiiton = Tensor([0.5, 0.0, 0.5])
-    orientation = torch.eye(3)
-    q = ik_solver.solve(posiiton, orientation)
-    end_time = time.time()
-    print("IK solution:", q)
-    print(f"IK solved in {end_time - start_time:.4f} seconds")
+
+    for pos, expected_q in test_data.items():
+        pos_tensor = Tensor(pos)
+        q = ik_solver.solve(pos_tensor, torch.diag(torch.tensor([1, -1, -1])))
+        assert torch.allclose(q, expected_q, atol=1e-4), (
+            f"Test failed for position {pos}, \n expected {expected_q}, \n got {q}"
+        )
+        print(f"Test passed for position {pos}")
