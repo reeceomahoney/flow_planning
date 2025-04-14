0ae865d79689d5f7799c016e264473ab8be68f2e
Reece O'Mahoney <reece.omahoney3@gmail.com>
2025-04-14 11:20:47 +0100

    add checkpoints

--- flow_planning/runner.py
+++ flow_planning/runner.py
@@ -179,8 +179,8 @@ class Runner:
                 iter_time = stop - start
 
                 self.log(locals())
-                if it % self.cfg.eval_interval == 0:
-                    self.save(os.path.join(self.log_dir, "models", "model.pt"))
+                if it % self.cfg.save_interval == 0:
+                    self.save(os.path.join(self.log_dir, "models", f"model_{it}.pt"))
 
         if self.log_dir is not None:
             self.save(os.path.join(self.log_dir, "models", "model.pt"))
