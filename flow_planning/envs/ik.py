1848e977e7b4fae85ff81900a378462e74ba1bc8
Reece O'Mahoney <reece.omahoney3@gmail.com>
2025-04-27 17:32:34 +0100

    remove ik controller

--- flow_planning/envs/ik.py
+++ flow_planning/envs/ik.py
@@ -1,10 +1,8 @@
 from flow_planning.envs.mdp.actions import CustomOscActionCfg
 from isaaclab.controllers import OperationalSpaceControllerCfg
-from isaaclab.envs.mdp.actions.actions_cfg import DifferentialInverseKinematicsActionCfg
 from isaaclab.utils import configclass
-from isaaclab_assets.robots.franka import FRANKA_PANDA_HIGH_PD_CFG
+from isaaclab_assets.robots.franka import FRANKA_PANDA_CFG
 
-from . import mdp
 from .rsl_rl import T_MAX, FrankaRLEnvCfg
 
 
@@ -13,39 +11,28 @@ class FrankaIKEnvCfg(FrankaRLEnvCfg):
     def __post_init__(self):
         super().__post_init__()
 
-        # FRANKA_PANDA_CFG.spawn.rigid_props.disable_gravity = False  # type: ignore
-        # FRANKA_PANDA_CFG.actuators["panda_shoulder"].stiffness = 0.0
-        # FRANKA_PANDA_CFG.actuators["panda_shoulder"].damping = 0.0
-        # FRANKA_PANDA_CFG.actuators["panda_forearm"].stiffness = 0.0
-        # FRANKA_PANDA_CFG.actuators["panda_forearm"].damping = 0.0
+        FRANKA_PANDA_CFG.spawn.rigid_props.disable_gravity = False  # type: ignore
+        FRANKA_PANDA_CFG.actuators["panda_shoulder"].stiffness = 0.0
+        FRANKA_PANDA_CFG.actuators["panda_shoulder"].damping = 0.0
+        FRANKA_PANDA_CFG.actuators["panda_forearm"].stiffness = 0.0
+        FRANKA_PANDA_CFG.actuators["panda_forearm"].damping = 0.0
 
-        self.scene.robot = FRANKA_PANDA_HIGH_PD_CFG.replace(prim_path="{ENV_REGEX_NS}/Robot")  # type: ignore
-        self.actions.arm_action = mdp.CustomIKActionCfg(
+        self.scene.robot = FRANKA_PANDA_CFG.replace(prim_path="{ENV_REGEX_NS}/Robot")  # type: ignore
+        self.actions.arm_action = CustomOscActionCfg(
             asset_name="robot",
             joint_names=["panda_joint.*"],
             body_name="panda_hand",
-            controller=mdp.CustomIKControllerCfg(
-                command_type="pose", use_relative_mode=False, ik_method="dls"
+            controller_cfg=OperationalSpaceControllerCfg(
+                target_types=["pose_abs"],
+                impedance_mode="fixed",
+                inertial_dynamics_decoupling=True,
+                partial_inertial_dynamics_decoupling=False,
+                gravity_compensation=True,
+                motion_stiffness_task=2.5,
+                motion_damping_ratio_task=1.0,
+                nullspace_control="position",
             ),
-            body_offset=DifferentialInverseKinematicsActionCfg.OffsetCfg(
-                pos=(0.0, 0.0, 0.107)
-            ),
+            nullspace_joint_pos_target="default",
+            body_offset=CustomOscActionCfg.OffsetCfg(pos=(0.0, 0.0, 0.107)),
         )
-        # self.actions.arm_action = CustomOscActionCfg(
-        #     asset_name="robot",
-        #     joint_names=["panda_joint.*"],
-        #     body_name="panda_hand",
-        #     controller_cfg=OperationalSpaceControllerCfg(
-        #         target_types=["pose_abs"],
-        #         impedance_mode="fixed",
-        #         inertial_dynamics_decoupling=True,
-        #         partial_inertial_dynamics_decoupling=False,
-        #         gravity_compensation=True,
-        #         motion_stiffness_task=2.5,
-        #         motion_damping_ratio_task=1.0,
-        #         nullspace_control="position",
-        #     ),
-        #     nullspace_joint_pos_target="default",
-        #     body_offset=CustomOscActionCfg.OffsetCfg(pos=(0.0, 0.0, 0.107)),
-        # )
         # self.episode_length_s = 2 * T_MAX
--- flow_planning/envs/mdp/actions.py
+++ flow_planning/envs/mdp/actions.py
@@ -2,12 +2,9 @@
 
 import torch
 
-from isaaclab.controllers.differential_ik import DifferentialIKController
-from isaaclab.controllers.differential_ik_cfg import DifferentialIKControllerCfg
 from isaaclab.envs.manager_based_env import ManagerBasedEnv
 from isaaclab.envs.mdp.actions import actions_cfg
 from isaaclab.envs.mdp.actions.task_space_actions import (
-    DifferentialInverseKinematicsAction,
     OperationalSpaceControllerAction,
 )
 from isaaclab.utils import configclass
@@ -58,60 +55,3 @@ class CustomOscAction(OperationalSpaceControllerAction):
         self._asset.set_joint_effort_target(
             self._joint_efforts, joint_ids=self._joint_ids
         )
-
-
-class CustomIKController(DifferentialIKController):
-    def compute(
-        self,
-        ee_pos: torch.Tensor,
-        ee_quat: torch.Tensor,
-        jacobian: torch.Tensor,
-        joint_pos: torch.Tensor,
-    ) -> torch.Tensor:
-        joint_pos_des = super().compute(ee_pos, ee_quat, jacobian, joint_pos)
-        # apply the custom scaling factor
-        delta_joint_pos = joint_pos_des - joint_pos
-        joint_pos_des = joint_pos + 0.15 * delta_joint_pos
-        joint_pos_des += 0.2 * torch.randn_like(joint_pos_des)
-        return joint_pos_des
-
-
-class CustomIKAction(DifferentialInverseKinematicsAction):
-    def __init__(
-        self,
-        cfg: actions_cfg.DifferentialInverseKinematicsActionCfg,
-        env: ManagerBasedEnv,
-    ):
-        super().__init__(cfg, env)
-        self._ik_controller = CustomIKController(
-            cfg=self.cfg.controller, num_envs=self.num_envs, device=self.device
-        )
-
-    def apply_actions(self):
-        # obtain quantities from simulation
-        ee_pos_curr, ee_quat_curr = self._compute_frame_pose()
-        joint_pos = self._asset.data.joint_pos[:, self._joint_ids]
-        # compute the delta in joint-space
-        if ee_quat_curr.norm() != 0:
-            jacobian = self._compute_frame_jacobian()
-            joint_pos_des = self._ik_controller.compute(
-                ee_pos_curr, ee_quat_curr, jacobian, joint_pos
-            )
-        else:
-            joint_pos_des = joint_pos.clone()
-        # set the joint position command
-        self.joint_pos_des = joint_pos_des
-        self._asset.set_joint_position_target(joint_pos_des, self._joint_ids)
-
-
-@configclass
-class CustomIKControllerCfg(DifferentialIKControllerCfg):
-    def __post_init__(self):
-        super().__post_init__()
-        self.class_type = CustomIKController
-
-
-@configclass
-class CustomIKActionCfg(actions_cfg.DifferentialInverseKinematicsActionCfg):
-    def __post_init__(self):
-        self.class_type = CustomIKAction
